edc7de789bc19b20f7c56d3c8b1589ce1438cd56
Jevioxy <t50_n.a.berlikov@mpt.ru>
2023-09-29 18:31:33 +0300

    практическая №2

---
+++ De_Serialize.cs
@@ -0,0 +1,31 @@
+﻿using Org.BouncyCastle.Utilities.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Wpf_Diary
+{
+    internal class De_Serialize
+    {
+        public static void Serialize<T>(T obj)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            using (var fs = new FileStream(path: @"..\..\..\db.xml", FileMode.Create))
+            {
+                xml.Serialize(fs, obj);
+            }
+        }
+        public static T Deserialize<T>()
+        {
+            T obj;
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            using (var fs = new FileStream(path: @"..\..\..\db.xml", FileMode.Open))
+            {
+                obj = (T?)xml.Deserialize(fs);
+            }
+            return obj;
+        }
+    }
+}
